349495936ec503d30014ad97456d3edadf8e3e42
Elnenedelguion <scriiptkiid@gmail.com>
2024-05-12 14:17:46 -0300

    se realizan cambios en la carpeta masc ubicada en el Proyecto_Masc

--- back/Proyecto_Masc/masc/asgi.py
+++ back/Proyecto_Masc/masc/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for Proyecto_Masc.
+ASGI config for masc project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Proyecto_Masc.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'masc.settings')
 
 application = get_asgi_application()
--- back/Proyecto_Masc/masc/wsgi.py
+++ back/Proyecto_Masc/masc/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for Proyecto_Masc.
+WSGI config for for masc project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
