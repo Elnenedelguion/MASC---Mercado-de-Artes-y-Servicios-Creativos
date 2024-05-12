769720728a57dd4f6e3cddd7293983f66211eb1b
Elnenedelguion <scriiptkiid@gmail.com>
2024-05-12 13:35:16 -0300

    se cambian config respecto al reniombramiento del proyecto

--- back/Proyecto_Masc/masc/wsgi.py
+++ back/Proyecto_Masc/masc/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for masc project.
+WSGI config for Proyecto_Masc.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'masc.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Proyecto_Masc.settings')
 
 application = get_wsgi_application()
