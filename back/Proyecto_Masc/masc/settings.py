b56934978bdcf0a5dd3f7d6e09b2b38aaa98ba79
Elnenedelguion <scriiptkiid@gmail.com>
2024-05-12 17:43:01 -0300

    se corrigen errores en settings.py

--- back/Proyecto_Masc/masc/settings.py
+++ back/Proyecto_Masc/masc/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'modelos.apps.ModelosConfig',
+    'Aplicaciones.modelos.apps.ModelosConfig',
 ]
 
 MIDDLEWARE = [
