1e486263e887f689bde786cf7adb08b20d621e61
Elnenedelguion <scriiptkiid@gmail.com>
2024-05-12 17:58:13 -0300

    se realizan cambios en apps.py

--- back/Proyecto_Masc/Aplicaciones/modelos/apps.py
+++ back/Proyecto_Masc/Aplicaciones/modelos/apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class ModelosConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'modelos'
+    name = 'aplicaciones.modelos'
