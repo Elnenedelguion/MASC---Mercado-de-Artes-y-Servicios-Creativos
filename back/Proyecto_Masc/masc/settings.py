dfa76b3514b1612b03ea870dc37824e1544d3748
Elnenedelguion <scriiptkiid@gmail.com>
2024-05-12 17:28:52 -0300

    se corrige INSTALLED_APPS

--- back/Proyecto_Masc/masc/settings.py
+++ back/Proyecto_Masc/masc/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'Aplicaciones.modelos',
+    'modelos.apps.ModelosConfig',
 ]
 
 MIDDLEWARE = [
