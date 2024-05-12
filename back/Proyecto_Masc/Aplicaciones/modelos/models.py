bb7785ab41c12d03e4c90ab92ac41eb92ceb40c9
Elnenedelguion <scriiptkiid@gmail.com>
2024-05-12 16:32:32 -0300

    Se crea carpeta aplicaciones dentro del Proyecto_Masc, luego el archivo que contiene los modelos de datos, Aplicaciones.modelos

---
+++ back/Proyecto_Masc/Aplicaciones/modelos/models.py
@@ -0,0 +1,53 @@
+from django.db import models
+from django.core.validators import RegexValidator
+from django.contrib.auth.models import User
+from django.utils import timezone
+
+
+class Usuario (models.Model):
+    id_usuario = models.PositiveIntegerField()  
+    nombre = models.CharField(max_length=30)
+    email = models.EmailField (max_length=30)
+    contraseña = models.CharField (max_length=30)
+    token = models.CharField (max_length=30, validators=[RegexValidator(regex='^[a-zA-Z0-9]*$', message='Solo letras y números están permitidos.')])
+    bio = models.TextField(blank=True)
+    fotodeperfil = models.ImageField(upload_to='profile_pics/', blank=True)
+    fecha_creacion_usuario = models.DateTimeField(default=timezone.now)
+
+  
+class Mensaje (models.Model):
+    id_mensaje= models.PositiveIntegerField()  
+    remitente = models.ForeignKey(User, related_name='sent_messages', on_delete=models.CASCADE)
+    destinatario = models.ForeignKey(User, related_name='received_messages', on_delete=models.CASCADE)
+    asunto = models.CharField(max_length=500)
+    body = models.TextField()
+    timestamp = models.DateTimeField(auto_now_add=True)
+    fecha_creacion_mensaje = models.DateTimeField(default=timezone.now)
+
+class Carrito (models.Model):
+    id_carrito= models.PositiveIntegerField()  
+    producto = models.CharField(max_length=100)  
+    total = models.DecimalField(max_digits=10, decimal_places=2)
+
+class Pago(models.Model):
+    id_producto = models.PositiveIntegerField()  
+    fecha = models.DateTimeField(default=timezone.now)
+  
+class Categoria (models.Model):
+    id_categoria = models.PositiveIntegerField()  
+    nombre_categoria = models.CharField(max_length=100)
+    fecha_creacion_categoria = models.DateTimeField(default=timezone.now)
+
+class Subcategoria (models.Model):
+    id_subcategoria = models.PositiveIntegerField()  
+    nombre_subcategoria = models.CharField(max_length=100)
+    fecha_creacion_subcategoria = models.DateTimeField(default=timezone.now)
+
+class Producto(models.Model):
+    categoria = models.ForeignKey(Categoria, related_name='productos', on_delete=models.CASCADE)
+    subcategoria = models.ForeignKey(Subcategoria, related_name='productos', on_delete=models.CASCADE)
+    nombre_producto = models.CharField(max_length=100)
+    descripcion = models.TextField()
+    precio = models.DecimalField(max_digits=10, decimal_places=2)
+    fecha_creacion_producto = models.DateTimeField(default=timezone.now)
+
--- back/Proyecto_Masc/masc/settings.py
+++ back/Proyecto_Masc/masc/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    "Aplicaciones.modelos"
 ]
 
 MIDDLEWARE = [
@@ -77,7 +78,7 @@ WSGI_APPLICATION = 'masc.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': 'masc.sqlite3',
     }
 }
 
