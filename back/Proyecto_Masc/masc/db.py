d465dc3a0a5b2f9f51c591c3c7f096458569908d
Diegog <diegogaray1987@gmail.com>
2024-05-13 23:11:49 -0300

    se importa la bd a settings.py

---
+++ back/Proyecto_Masc/masc/db.py
@@ -0,0 +1,11 @@
+MySQL={
+
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'MASC',
+        'USER': 'root',
+        'PASSWORD': 'root',
+        'HOST': 'localhost',
+        'PORT': '3306',
+    },
+}
--- back/Proyecto_Masc/masc/settings.py
+++ back/Proyecto_Masc/masc/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+from .db import MYSQL 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -75,16 +76,10 @@ WSGI_APPLICATION = 'masc.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'MASC',
-        'USER': 'root',
-        'PASSWORD': 'root',
-        'HOST': 'localhost',
-        'PORT': '3306',
-    },
+DATABASES = MySQL{
+    
 }
+
 
 
 
