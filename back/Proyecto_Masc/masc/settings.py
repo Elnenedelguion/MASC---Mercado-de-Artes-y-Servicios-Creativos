f5c3b4f7c4ade75f556c0f5b0d210615001fb380
Elnenedelguion <scriiptkiid@gmail.com>
2024-05-13 23:33:31 -0300

    correciones en settings.py

--- back/Proyecto_Masc/masc/settings.py
+++ back/Proyecto_Masc/masc/settings.py
@@ -11,7 +11,8 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
-from .db import MYSQL 
+from .db import MySQL
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
