5ba47556c65dda74c5e864adfd8f74becee09f2c
Elnenedelguion <scriiptkiid@gmail.com>
2024-05-23 05:11:54 -0300

    Se define las vistas para el carrito de compras

--- back/Proyecto_Masc/Aplicaciones/modelos/views.py
+++ back/Proyecto_Masc/Aplicaciones/modelos/views.py
@@ -3,8 +3,22 @@ from django.shortcuts import render
 from rest_framework import viewsets
 from .models import Producto
 from .serializers import ProductoSerializer
+from .models import Carrito
+from .models import CarritoProducto
+from .serializers  import CarritoSerializer
+from .serializers  import CarritoProductoSerializer
+
 
 class ProductoViewSet(viewsets.ModelViewSet):
     queryset = Producto.objects.all()
     serializer_class = ProductoSerializer
+
+class CarritoViewSet(viewsets.ModelViewSet):
+    queryset = Carrito.objects.all()
+    serializer_class = CarritoSerializer
+
+class CarritoProductoViewSet(viewsets.ModelViewSet):
+    queryset = CarritoProducto.objects.all()
+    serializer_class = CarritoProductoSerializer
+
 
