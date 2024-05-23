6abdcc0849ad56065838aa50b60089ae0251cbc4
Elnenedelguion <scriiptkiid@gmail.com>
2024-05-23 04:24:01 -0300

    Defino las vistas para la API:

--- back/Proyecto_Masc/Aplicaciones/modelos/views.py
+++ back/Proyecto_Masc/Aplicaciones/modelos/views.py
@@ -1,3 +1,10 @@
+# masc/views.py
 from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Producto
+from .serializers import ProductoSerializer
 
-# Create your views here.
+class ProductoViewSet(viewsets.ModelViewSet):
+    queryset = Producto.objects.all()
+    serializer_class = ProductoSerializer
+
