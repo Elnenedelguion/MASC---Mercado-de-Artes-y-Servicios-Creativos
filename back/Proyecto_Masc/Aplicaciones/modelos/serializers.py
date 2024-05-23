bcfb4d6246919641958323d06de98c47468c6c8e
Elnenedelguion <scriiptkiid@gmail.com>
2024-05-23 05:05:35 -0300

    Funcionalidad del carrito compras. Se define los modelos y serializadores para el carrito de compras en el archivo serializers.py

--- back/Proyecto_Masc/Aplicaciones/modelos/serializers.py
+++ back/Proyecto_Masc/Aplicaciones/modelos/serializers.py
@@ -1,8 +1,24 @@
 # masc/serializers.py
 from rest_framework import serializers
 from .models import Producto
+from .models import Carrito
+from .models import CarritoProducto
+
+
 
 class ProductoSerializer(serializers.ModelSerializer):
     class Meta:
         model = Producto
+        fields = '__all__'
+
+class CarritoProductoSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = CarritoProducto
+        fields = '__all__'
+
+class CarritoSerializer(serializers.ModelSerializer):
+    productos = CarritoProductoSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = Carrito
         fields = '__all__'
