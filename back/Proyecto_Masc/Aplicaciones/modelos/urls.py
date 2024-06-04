b2505ef6fcc128e2065317be70418cdec4b01d6f
Maria Alejandra Rodriguez <87164629+Marialer27@users.noreply.github.com>
2024-06-03 22:33:11 -0300

    Update urls.py

--- back/Proyecto_Masc/Aplicaciones/modelos/urls.py
+++ back/Proyecto_Masc/Aplicaciones/modelos/urls.py
@@ -1,8 +1,11 @@
+from django.contrib import admin
+from django.urls import path, include
 from django.urls import path
 from rest_framework.routers import DefaultRouter
 from .views import RegisterView, LoginView, LogoutView, UsuarioViewSet, PagoViewSet, CategoriaViewSet, SubcategoriaViewSet, ProductoViewSet, CarritoProductoViewSet, HistorialCarritoViewSet, FacturacionViewSet, index, dashboard, login_view, register_view
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 from Aplicaciones.modelos.views import index, dashboard, login_view, register_view, logout_view
+from .views import UsuarioListView
 
 
 
@@ -20,17 +23,16 @@ router.register(r'facturaciones', FacturacionViewSet)
 
 # Definir los patrones de URL
 urlpatterns = [
-    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-    path('api/register/', RegisterView.as_view(), name='auth_register'),
-    path('api/login/', LoginView.as_view(), name='auth_login'),
-    path('api/logout/', LogoutView.as_view(), name='auth_logout'),
-    path('', index, name='index'),
-    path('dashboard/', dashboard, name='dashboard'),
-    path('login/', login_view, name='login'),
-    path('register/', register_view, name='register'),
-    path('logout/', logout_view, name='logout'),
+    path('api/usuarios/', UsuarioListView.as_view(), name='usuario-list'),
+     path('admin/', admin.site.urls),
+    path('', include(router.urls)),
+    path('usuarios/login/', LoginView.as_view(), name='login'),
+    path('usuarios/logout/', LogoutView.as_view(), name='logout'),
+    path('usuarios/register/', RegisterView.as_view(), name='register'),
  
  
 ]
+
 
