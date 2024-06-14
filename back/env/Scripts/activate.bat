60b788a95d2a2ffb08efc157cb60e2f6037ad054
Marialer27 <maria.alerodriguez@gmail.com>
2024-06-14 15:31:31 -0300

    Agregamos carrito

--- back/env/Scripts/activate.bat
+++ back/env/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\HP\Downloads\MOD-PROGRAM-WEB-2024 3\MOD-PROGRAM-WEB-2024\back\env
+set VIRTUAL_ENV=C:\Users\aleja\Downloads\MASC2024\MOD-PROGRAM-WEB-2024\back\env
 
 if not defined PROMPT set PROMPT=$P$G
 
